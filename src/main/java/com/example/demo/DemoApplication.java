e4dfade298eff1955397fd10d3e8e251b8587c54
KrishanHimesh <141705132+KrishanHimesh@users.noreply.github.com>
2024-03-18 11:30:56 +1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World! My Name is Krishan";
+		return "Hello World! My Name is Krishan Himesh Abeyrethne";
 	}
 }
